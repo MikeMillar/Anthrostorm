2bb1e3638484c6ae4cc6ab8da923427b96384d5d
Michael Millar <mcmelord@gmail.com>
2022-08-20 04:15:13 -0600

    Initial Commit
    Initial Project Commit

---
+++ cm-client/src/main/java/net/anthrostorm/client/Anthrostorm.java
@@ -0,0 +1,12 @@
+package net.anthrostorm.client;
+
+/**
+ * created by: author: MichaelMillar date: 8/11/2022
+ */
+public class Anthrostorm {
+
+  public static void main(String[] args) {
+
+  }
+
+}
