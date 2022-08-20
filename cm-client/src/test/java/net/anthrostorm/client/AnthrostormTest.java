603445a8d5eacf64e104d2872fac2b48cbc317b1
Michael Millar <mcmelord@gmail.com>
2022-08-20 05:10:30 -0600

    Add Test Configurations
    Add Test configurations for future use

---
+++ cm-client/src/test/java/net/anthrostorm/client/AnthrostormTest.java
@@ -0,0 +1,15 @@
+package net.anthrostorm.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AnthrostormTest {
+
+  @Test
+  void testJunit() {
+    assertEquals("Hello, World!", "Hello, World!");
+  }
+
+
+}
